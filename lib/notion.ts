23ba8c691bcfc5c22b1a5ebb066decfa3b09779c
hqhanqizhq <hanqizenghannanana@gmail.com>
2026-04-19 16:22:05 -0400

    fix recordmap error

--- lib/notion.ts
+++ lib/notion.ts
@@ -43,7 +43,7 @@ const getNavigationLinkPages = pMemoize(
 )
 
 export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
-  await new Promise((resolve) => setTimeout(resolve, 20000))
+  let recordMap = await notion.getPage(pageId)
 
   if (navigationStyle !== 'default') {
     // ensure that any pages linked to in the custom navigation header have
